69dfaa89ebf6e98fa5ef0c27c10625d20cd8d967
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-04-29 09:15:36 -0700

    Add footer component

---
+++ src/Components/Footer.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Footer.styles.css';
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p>&copy; 2022 | made By Akaki Burjanadze</p>
+        </footer>
+    );
+}
+
+export default Footer;
